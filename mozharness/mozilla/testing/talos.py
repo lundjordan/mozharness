64512b71f887d6dcbac8dd30b7808ff5501fc22a
Jordan Lund <jordan.l.lund@gmail.com>
2013-08-07 18:13:04 -0700

    removed --metro-immersive option and now fire metro mode based on suite name

--- mozharness/mozilla/testing/talos.py
+++ mozharness/mozilla/testing/talos.py
@@ -92,11 +92,11 @@ class Talos(TestingMixin, MercurialScript):
            "dest": "branch",
            "help": "Graphserver branch to report to"
            }],
-        [["--metro-immersive"],
-          {"action": "store_true",
-           "dest": "metro_immersive",
-           "help": "Tells windows 8 machines to run tests with Metro Browser"
-           }],
+        # [["--metro-immersive"],
+        #   {"action": "store_true",
+        #    "dest": "metro_immersive",
+        #    "help": "Tells windows 8 machines to run tests with Metro Browser"
+        #    }],
         [["--system-bits"],
           {"action": "store",
            "dest": "system_bits",
@@ -157,6 +157,20 @@ class Talos(TestingMixin, MercurialScript):
         self.pagesets_manifest_parent_path = None
         if 'run-tests' in self.actions:
             self.preflight_run_tests()
+        if self.config.get('suite').endswith('-metro'):
+            # TEMPORARY CODE: this script takes a `suite` option that is used as a
+            # key from a talos_json_url
+            # (eg: http://hg.mozilla.org/mozilla-central/raw-file/s(revision)/testing/talos/talos.json)
+            # if we pass a suite with a '-metro' suffix through buildbot or a developer,
+            # the talos.json file in m-c won't know about it yet.
+            # Until we modify that json file in m-c to add win metro based keys/values,
+            # let's just capture the fact we want 'metro_immersive' then use the
+            # non metro equivalent from the talos.json file
+            # (eg: self.talos_json_config['suites']['dromaeojs'])
+            self.config['suite'] = self.config['suite'].replace('-metro', '')
+            self.config['metro_immersive'] = True
+
+
 
     def query_abs_dirs(self):
         c = self.config

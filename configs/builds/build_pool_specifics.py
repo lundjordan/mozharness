10d24c53131b7d8b3630a7a675a5c10c65f11ccb
Jordan Lund <jordan.l.lund@gmail.com>
2014-03-16 18:17:41 -0700

    rm duplicate reset_mock, preproduction parts, testresults, and adds type to balrog

--- configs/builds/build_pool_specifics.py
+++ configs/builds/build_pool_specifics.py
@@ -7,6 +7,7 @@ config = {
         # XXX: should point at aus4-admin-dev once production is pointing
         # elsewhere
         'balrog_api_root': 'https://aus4-admin-dev.allizom.org',
+        'balrog_username': 'stage-ffxbld',
         'download_base_url': 'http://dev-stage01.srv.releng.scl3.mozilla'
                              '.com/pub/mozilla.org/firefox/nightly',
         'aus2_ssh_key': 'ffxbld_dsa',
@@ -14,7 +15,7 @@ config = {
         'aus2_host': 'dev-stage01.srv.releng.scl3.mozilla.com',
         'stage_server': 'dev-stage01.srv.releng.scl3.mozilla.com',
         'symbol_server_host': "dev-stage01.srv.releng.scl3.mozilla.com",
-        # staging/preproduction we should use MozillaTest
+        # staging we should use MozillaTest
         # but in production we let the self.branch decide via
         # self._query_graph_server_branch_name()
         "graph_server_branch_name": "MozillaTest",
@@ -23,34 +24,10 @@ config = {
             '.org/firefox/bundles',
         ],
     },
-    # I don't think we use preproduction anymore, but it was little effort to
-    #  add it
-    "preproduction": {
-        "sendchange_masters": [
-            "preproduction-master.srv.releng.scl3.mozilla.com:9008"
-        ],
-        # XXX: should point at aus4-admin-dev once production is pointing
-        # elsewhere
-        'balrog_api_root': 'https://aus4-admin-dev.allizom.org',
-        'download_base_url': 'http://preproduction-stage.srv.releng.scl3.\
-mozilla.com/pub/mozilla.org/firefox/nightly',
-        'aus2_host': 'preproduction-stage.srv.releng.scl3.mozilla.com',
-        'aus2_ssh_key': 'cltbld_dsa',
-        'aus2_user': 'cltbld',
-        'stage_server': 'preproduction-stage.srv.releng.scl3.mozilla.com',
-        'symbol_server_host': "preproduction-stage.srv.releng.scl3.mozilla.com",
-        # staging/preproduction we should use MozillaTest
-        # but in production we let the self.branch decide via
-        # self._query_graph_server_branch_name()
-        "graph_server_branch_name": "MozillaTest",
-        "hgtool_base_bundle_urls": [
-            'http://preproduction-master.srv.releng.scl3.mozilla'
-            '.com/pub/mozilla.org/firefox/bundles',
-        ],
-    },
     "production": {
         "sendchange_masters": ["buildbot-master81.build.mozilla.org:9301"],
         'balrog_api_root': 'https://aus4-admin.mozilla.org',
+        'balrog_username': 'ffxbld',
         'download_base_url': 'http://ftp.mozilla.org/pub/mozilla'
                              '.org/firefox/nightly',
         'aus2_host': 'aus3-staging.mozilla.org',

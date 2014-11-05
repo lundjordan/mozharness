2f296216c9c9603567ed099f713b14ff27fd72b1
Jordan Lund <jordan.l.lund@gmail.com>
2014-11-04 16:59:14 -0800

    dev work to disable pgo for nightly builds

--- mozharness/mozilla/building/buildbase.py
+++ mozharness/mozilla/building/buildbase.py
@@ -799,10 +799,11 @@ or run without that action (ie: --no-{action})"
             a) must be a nightly build
             b) must be on a branch that runs pgo if it can everytime
         """
-        c = self.config
-        if self.stage_platform in c['pgo_platforms']:
-            if c.get('branch_uses_per_checkin_strategy') or self.query_is_nightly():
-                return True
+        # XXX JLUND DISABLES PGO FOR NIGHTLIES AND PER-CHECKIN
+        # c = self.config
+        # if self.stage_platform in c['pgo_platforms']:
+        #     if c.get('branch_uses_per_checkin_strategy') or self.query_is_nightly():
+        #         return True
         return False
 
     def query_check_test_env(self):
